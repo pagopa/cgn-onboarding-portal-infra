3e5a4591aba28e4164a0994ce6e809ea70eff46e
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-04-12 15:18:02 +0200

    map var

--- vars/dev.tf
+++ vars/dev.tf
@@ -6,3 +6,18 @@ resource "tfe_variable" "dev_env_short" {
   workspace_id = data.tfe_workspace.dev.id
   description  = "Single char for the current environment"
 }
+
+
+resource "tfe_variable" "dev_tags" {
+  key          = "tags"
+  value        = <<-EOT
+  {
+    CreateBy    = "Terraform"
+    Environment = "Dev"
+  }
+  EOT
+  hcl          = true
+  category     = "terraform"
+  workspace_id = data.tfe_workspace.dev.id
+  description  = "Single char for the current environment"
+}
