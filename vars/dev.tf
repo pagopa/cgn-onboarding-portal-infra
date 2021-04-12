200d8e6e432eef0782bf6dbf8e604ed1f6aff9fd
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-04-12 17:27:24 +0200

    update vars

--- vars/dev.tf
+++ vars/dev.tf
@@ -21,3 +21,51 @@ resource "tfe_variable" "dev_tags" {
   workspace_id = data.tfe_workspace.dev.id
   description  = "tags for the cloud resources"
 }
+
+
+# Network
+resource "tfe_variable" "dev_cidr_vnet" {
+  key          = "cidr_vnet"
+  value        = <<-EOT
+  ["10.0.0.0/16"]
+  EOT
+  hcl          = true
+  category     = "terraform"
+  workspace_id = data.tfe_workspace.dev.id
+  description  = "cidr virtual network"
+}
+
+resource "tfe_variable" "dev_cidr_subnet_db" {
+  key          = "cidr_subnet_db"
+  value        = <<-EOT
+  ["10.0.1.0/24"]
+  EOT
+  hcl          = true
+  category     = "terraform"
+  workspace_id = data.tfe_workspace.dev.id
+  description  = "cidr subnet db"
+}
+
+resource "tfe_variable" "dev_cidr_subnet_api" {
+  key          = "cidr_subnet_api"
+  value        = <<-EOT
+  ["10.0.2.0/24"]
+  EOT
+  hcl          = true
+  category     = "terraform"
+  workspace_id = data.tfe_workspace.dev.id
+  description  = "cidr virtual api"
+}
+
+resource "tfe_variable" "dev_cidr_subnet_public" {
+  key          = "cidr_subnet_public"
+  value        = <<-EOT
+  ["10.0.3.0/24"]
+  EOT
+  hcl          = true
+  category     = "terraform"
+  workspace_id = data.tfe_workspace.dev.id
+  description  = "cidr subnet public"
+}
+
+
