272c1ff872fee51c78976240497012353162a10d
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-04-13 15:20:13 +0200

    private

--- code/storage.tf
+++ code/storage.tf
@@ -28,9 +28,10 @@ module "storage_account" {
 
 # Containers
 resource "azurerm_storage_container" "user_documents" {
+  depends_on            = [module.storage_account]
   name                  = "userdocuments"
   storage_account_name  = module.storage_account.name
-  container_access_type = "blob"
+  container_access_type = "private"
 }
 
 
