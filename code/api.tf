a223ed5f387e5df9e979dbea123ac9b3e82950a4
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-04-14 10:26:21 +0200

    Not needed

--- code/api.tf
+++ code/api.tf
@@ -10,7 +10,6 @@ resource "azurerm_container_registry" "container_registry" {
   resource_group_name = azurerm_resource_group.rg_api.name
   location            = azurerm_resource_group.rg_api.location
   sku                 = var.sku_container_registry
-  admin_enabled       = true
 
 
   dynamic "retention_policy" {
