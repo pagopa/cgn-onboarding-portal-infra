7d39d0897b78bcaf1a0bb6f96cfb5ff5db82b989
pasqualedevita <>
2021-05-12 16:40:18 +0200

    fix dns uat

--- code/dns.tf
+++ code/dns.tf
@@ -8,13 +8,21 @@ resource "azurerm_dns_zone" "public" {
   tags = var.tags
 }
 
+resource "azurerm_dns_zone" "public_uat" {
+  count               = (var.dns_zone_prefix_uat == null || var.external_domain == null) ? 0 : 1
+  name                = join(".", [var.dns_zone_prefix_uat, var.external_domain])
+  resource_group_name = azurerm_resource_group.rg_public.name
+
+  tags = var.tags
+}
+
 resource "azurerm_dns_a_record" "api" {
   count               = var.enable_custom_dns ? 1 : 0
   name                = "api"
   records             = [azurerm_public_ip.apigateway_public_ip.ip_address]
   resource_group_name = azurerm_resource_group.rg_public.name
   ttl                 = 300
-  zone_name           = azurerm_dns_zone.public[0].name
+  zone_name           = var.dns_zone_prefix != null ? azurerm_dns_zone.public[0].name : azurerm_dns_zone.public_uat[0].name
 
   tags = var.tags
 }
@@ -25,40 +33,7 @@ resource "azurerm_dns_cname_record" "frontend" {
   record              = module.cdn_portal_frontend.hostname
   resource_group_name = azurerm_resource_group.rg_public.name
   ttl                 = 300
-  zone_name           = azurerm_dns_zone.public[0].name
-
-  tags = var.tags
-}
-
-# TODO
-# these _uat are a temponary resources
-
-resource "azurerm_dns_zone" "public_uat" {
-  count               = (var.dns_zone_prefix_uat == null || var.external_domain == null) ? 0 : 1
-  name                = join(".", [var.dns_zone_prefix_uat, var.external_domain])
-  resource_group_name = azurerm_resource_group.rg_public.name
-
-  tags = var.tags
-}
-
-resource "azurerm_dns_a_record" "api_uat" {
-  count               = var.enable_custom_dns ? 1 : 0
-  name                = "api"
-  records             = [azurerm_public_ip.apigateway_public_ip.ip_address]
-  resource_group_name = azurerm_resource_group.rg_public.name
-  ttl                 = 300
-  zone_name           = azurerm_dns_zone.public_uat[0].name
-
-  tags = var.tags
-}
-
-resource "azurerm_dns_cname_record" "frontend_uat" {
-  count               = var.enable_custom_dns ? 1 : 0
-  name                = "portal"
-  record              = module.cdn_portal_frontend.hostname
-  resource_group_name = azurerm_resource_group.rg_public.name
-  ttl                 = 300
-  zone_name           = azurerm_dns_zone.public_uat[0].name
+  zone_name           = var.dns_zone_prefix != null ? azurerm_dns_zone.public[0].name : azurerm_dns_zone.public_uat[0].name
 
   tags = var.tags
 }
