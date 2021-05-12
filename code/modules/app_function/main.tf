9052a9bdb83230a7035f080cbf78c1d5205764c0
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2021-05-12 15:46:49 +0200

    Add temp uat public dns zone (#33)
    * Add temp uat public dns zone
    
    * Add new env var and policy (#32)
    
    * Add new env var and policy
    
    * Add ACS_BASE_URL
    
    * Add temp uat public dns zone
    
    * tmp ignore
    
    Co-authored-by: pasqualedevita <>
    Co-authored-by: Matteo Gazzetta <matteo_gazzetta@hotmail.it>

--- code/modules/app_function/main.tf
+++ code/modules/app_function/main.tf
@@ -43,6 +43,8 @@ resource "azurerm_function_app" "this" {
   app_service_plan_id        = azurerm_app_service_plan.this.id
   storage_account_name       = azurerm_storage_account.this.name
   storage_account_access_key = azurerm_storage_account.this.primary_access_key
+  #tfsec:ignore:AZU028
+  https_only = false
 
   site_config {
     min_tls_version           = "1.2"
