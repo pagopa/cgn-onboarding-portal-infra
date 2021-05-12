fd4062a22e39d5abbd60b3384984b79687463eea
Matteo Gazzetta <matteo.gazzetta@bitrock.it>
2021-05-12 11:31:04 +0200

    Add new env var and policy

--- code/api.tf
+++ code/api.tf
@@ -153,7 +153,7 @@ module "spid_login" {
     ENABLE_JWT                         = "true"
     INCLUDE_SPID_USER_ON_INTROSPECTION = "true"
 
-    DEFAULT_TOKEN_EXPIRATION = "3600"
+    TOKEN_EXPIRATION = "3600"
     JWT_TOKEN_EXPIRATION     = "3600" # TODO remove when new version released
     JWT_TOKEN_ISSUER         = "SPID"
     JWT_TOKEN_PRIVATE_KEY    = tls_private_key.jwt.private_key_pem
@@ -163,6 +163,7 @@ module "spid_login" {
     ADE_AA_API_ENDPOINT = format("https://%s/adeaa/v1", var.app_gateway_host_name)
     ENDPOINT_L1_SUCCESS = format("https://%s/", module.cdn_portal_frontend.hostname)
     L1_TOKEN_EXPIRATION = 60
+    L1_TOKEN_HEADER_NAME = "X-CGN-TOKEN"
     L2_TOKEN_EXPIRATION = 3600
 
     # application insights key
