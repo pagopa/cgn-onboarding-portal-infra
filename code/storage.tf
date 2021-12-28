e643232b2e417770da065111c36b7867a6490611
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2021-12-28 12:30:01 +0100

    blob-versioning (#62)
    Co-authored-by: pasqualedevita <>

--- code/storage.tf
+++ code/storage.tf
@@ -83,6 +83,16 @@ resource "azurerm_storage_account" "ade_aa_mock" {
 
   account_replication_type = "LRS"
 
+  blob_properties {
+    versioning_enabled = true
+    delete_retention_policy {
+      days = 30
+    }
+    container_delete_retention_policy {
+      days = 30
+    }
+  }
+
   tags = var.tags
 }
 
